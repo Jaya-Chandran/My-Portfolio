0560fb2673ea92a0ea5bac65dd6e37fd765a85dd
Jaya-Chandran <jaichandranr28@gmail.com>
2026-09-06 21:04:23 +0530

    secrue admin password with env variable

--- build.sh
+++ build.sh
@@ -10,14 +10,15 @@ echo "
 import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
+password = os.environ.get('ADMIN_PASSWORD', 'NewPassword@123')
 if User.objects.filter(username='jai').exists():
     u = User.objects.get(username='jai')
-    u.set_password('jai1234')
+    u.set_password(password)
     u.is_staff = True
     u.is_superuser = True
     u.save()
     print('Password updated!')
 else:
-    User.objects.create_superuser('jai', 'your@email.com', 'jai1234')
+    User.objects.create_superuser('jai', 'your@email.com', password)
     print('Superuser created!')
 " | python manage.py shell
