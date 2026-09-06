a8535bb5bcd2a0e7bace3659dbfa36bb0595f2f4
Jaya-Chandran <jaichandranr28@gmail.com>
2026-09-06 20:57:54 +0530

    update password build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,17 @@ python manage.py migrate
 
 # Force create or update superuser password
 echo "
+import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if User.objects.filter(username='jai').exists():
     u = User.objects.get(username='jai')
-    u.set_password('NewPassword@123')
+    u.set_password('jai1234')
     u.is_staff = True
     u.is_superuser = True
     u.save()
     print('Password updated!')
 else:
-    User.objects.create_superuser('jai', 'your@email.com', 'NewPassword@123')
+    User.objects.create_superuser('jai', 'your@email.com', 'jai1234')
     print('Superuser created!')
 " | python manage.py shell
